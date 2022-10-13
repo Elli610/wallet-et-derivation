808322dddffc0d28a6da4a3972029158c21de459
Elli610 <113121626+Elli610@users.noreply.github.com>
2022-10-13 09:16:28 +0200

    start

---
+++ keys_and_derivation/target/debug/deps/keys_and_derivation-f334f487a86d7e09.d
@@ -0,0 +1,5 @@
+c:\Users\hervi\Documents\Cours ESILV\Rust\wallet-et-derivation\keys_and_derivation\target\debug\deps\keys_and_derivation-f334f487a86d7e09.rmeta: src\main.rs
+
+c:\Users\hervi\Documents\Cours ESILV\Rust\wallet-et-derivation\keys_and_derivation\target\debug\deps\keys_and_derivation-f334f487a86d7e09.d: src\main.rs
+
+src\main.rs:
