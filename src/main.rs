024d77280a0ff68062f1664657cd515d6d7d7e30
Elli610 <113121626+Elli610@users.noreply.github.com>
2022-10-20 10:44:55 +0200

    return master_priv and master_chaincode

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,30 @@ fn get_random_bytes() -> [u8; 16] {
     return random_bytes;
 }
    
-fn master_private_key(private_key : [u8; 16] ) {
-    let mut _master_private = [0u8; 16];
+fn master_private_key(private_key : [u8; 16] ) -> (Vec<u8>,Vec<u8>) {
 
     // Create alias for HMAC-SHA
     type HmacSha512 = Hmac<Sha512>;
 
     let mac = HmacSha512::new_from_slice(private_key.to_vec().as_slice()).unwrap().finalize().into_bytes();
+
+    // master_private_key
+    let master_private_key = mac[..32].to_vec();
+
+    // master_chaincode
+    let master_chaincode = mac[32..].to_vec();
+
     println!("mac: {:?}", mac);
-    println!("{:?}", mac[0])
+    println!("Master private key: {:?}", master_private_key);
+    println!("Master chain code: {:?}", master_chaincode);
     
+    (master_private_key,master_chaincode)
 }
 
 fn main() {
     let a = get_random_bytes();
     println!("Random bytes: {:?}", a);
-    master_private_key(a)
+    //(master_priv,master_chaincode) = master_private_key(a);
     //println!("Master private key: {:?}", master_private_key(a));
 
 }
