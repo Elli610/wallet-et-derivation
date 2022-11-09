01d4d453756bef87d406f3e65f57e32ad3c8c020
Elli610 <113121626+Elli610@users.noreply.github.com>
2022-11-09 08:53:08 +0100

    master_key function Updated
    Now, the outputs are 2 Strings instead of 2 u8 vectors

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,27 @@
 use rand::Rng;
 use sha2::Sha512;
 use hmac::{Hmac, Mac};
-//use hex_literal::hex;
 
-fn get_random_bytes() -> [u8; 16] {
+
+fn get_random_bytes() -> [u8; 16] { 
+    /*
+    *  This function returns a random 16 byte array
+    */
     let random_bytes = rand::thread_rng().gen::<[u8; 16]>();
 
     return random_bytes;
 }
-   
-fn master_private_key(private_key : [u8; 16] ) -> (Vec<u8>,Vec<u8>) {
+
+fn join_int(vect : Vec<u8>) -> String {
+    /* concatenate the elements of vect to build one String */
+    let out = vect.iter().map(|&x| x.to_string()).collect::<Vec<String>>().join("");
+    return out;
+}
+
+fn master_key(private_key : [u8; 16] ) -> (String,String) {
+    /* 
+    * extract the master private key and the master chaincode from the private key
+    */
 
     // Create alias for HMAC-SHA
     type HmacSha512 = Hmac<Sha512>;
@@ -22,19 +34,27 @@ fn master_private_key(private_key : [u8; 16] ) -> (Vec<u8>,Vec<u8>) {
 
     // master_chaincode
     let master_chaincode = mac[32..].to_vec();
-
+    
+    
     println!("mac: {:?}", mac);
     println!("Master private key: {:?}", master_private_key);
     println!("Master chain code: {:?}", master_chaincode);
     
-    (master_private_key,master_chaincode)
+
+    (join_int(master_private_key),join_int(master_chaincode))
+
+
 }
+
+
+
+
 
 fn main() {
     let a = get_random_bytes();
     println!("Random bytes: {:?}", a);
     //(master_priv,master_chaincode) = master_private_key(a);
-    //println!("Master private key: {:?}", master_private_key(a));
+    println!("Master private key: {:?}", master_key(a).0);
 
 }
 
